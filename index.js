a451ba82ed114931b040562d9f551536297b8fa4
VipGamer11 <90019482+VipGamer11@users.noreply.github.com>
2021-09-03 09:34:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const aoijs = require("aoi.js")
+
+const bot = new aoijs.Bot({
+token: "bot token", //Discord Bot Token
+prefix: "$getServerVar[prefix]" //Discord Bot Prefix
+})
+bot.onMessage() //Allows to execute Commands
+
+bot.variables({
+prefix: "PREFIX"
+  })
+
+bot.status({
+  text: "TEXT",
+  type: "PLAYING",
+  status: "dnd",
+  time: 12
+})
+
+bot.command({
+name: "ping", //Trigger name (command name)
+code: `Pong! $pingms` //Code
+});
+
+bot.readyCommand({
+    channel: "", //You can use this or not
+    code: `$log[Ready on $userTag[$clientID]]` //Example Ready on Client
+})
+
+bot.loadCommands(`./handler/`)
